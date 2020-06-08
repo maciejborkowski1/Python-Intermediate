2d66fbdaab775ed3a5af7d7689ca21aec2cfdc29
maciejborkowski1 <55588993+maciejborkowski1@users.noreply.github.com>
2020-06-08 20:52:54 +0200

    Update 02_25_enumerate_zip.py
    Correct first and second loop (redundant enumerate expression)

--- 02_25_enumerate_zip.py
+++ 02_25_enumerate_zip.py
@@ -1,15 +1,17 @@
 projects = ['Brexit', 'Nord Stream', 'US Mexico Border']
 leaders = ['Theresa May', 'Wladimir Putin', 'Donald Trump and Bill Clinton']
 
-for i, (j, z) in enumerate(zip(projects, leaders)):
+for i, (j, z) in zip(projects, leaders)):
     print('The leader of {} is {}.'.format(j, z))
 
 print ('-'*50)
 
 dates = ['2016-06-23', '2016-08-29', '1994-01-01']
 
-for i, (j, z, x) in enumerate(zip(projects, dates, leaders)):
+for i, (j, z, x) in zip(projects, dates, leaders)):
     print('The leader of {}, started {}, is {}.'.format(j, z, x))
+    
+print ('-'*50)
 
 for i, (j, z, x) in enumerate(zip(projects, dates, leaders)):
     print('{}: The leader of {}, started {}, is {}.'.format(i+1, j, z, x))
