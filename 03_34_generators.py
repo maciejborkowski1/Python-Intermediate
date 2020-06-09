1499faff8c0f0b2b962768150acc894679bd5538
maciejborkowski1 <55588993+maciejborkowski1@users.noreply.github.com>
2020-06-09 21:03:07 +0200

    Add files via upload

---
+++ 03_34_generators.py
@@ -0,0 +1,25 @@
+ports = ['WAW', 'KRK', 'GDN', 'KTW', 'WMI', 'WRO', 'POZ', 'RZE', 'SZZ', 'LUZ', 'BZG', 'LCJ', 'SZY', 'IEG', 'RDO']
+
+#to generate single value from list use expression 'next(flights)'
+
+flights = ((a,b) for a in ports for b in ports)
+x=1
+for i in flights:
+    x+=1
+print('We have generated {} elements'.format(x))
+
+print('-'*50)
+
+flights = ((a,b) for a in ports for b in ports if a != b)
+x=1
+for i in flights:
+    x+=1
+print('We have generated {} elements'.format(x))
+
+print('-'*50)
+
+flights = [(a,b) for a in ports for b in ports if a < b]
+x=1
+for i in flights:
+    x+=1
+print('We have generated {} elements'.format(x))
